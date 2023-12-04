5b6a5e2247f5cb54a20bda63189cee8b47073e09
luckychacha <358140284@qq.com>
2023-12-04 15:01:12 +0800

    add validation of address.

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,12 @@ fn main() {
     // The first way to generate an Ethereum address is to use the EthereumAddress type.
     let ethereum_address = EthereumAddress::new(private_key_hex);
     match ethereum_address {
-        Ok(ethereum_address) => println!("Address: 0x{}", ethereum_address.hex_encode_address()),
-        Err(e) => println!("Failed to generate Ethereum address: {}", e),
+        Ok(ethereum_address) => {
+            let address = format!("0x{}", ethereum_address.hex_encode_address());
+            assert_eq!(address, "0x18a5113f7fbc31e73c2bb38a895fd6683803e7f8");
+            println!("Address: {address}")
+        },
+        Err(e) => println!("Failed to generate Ethereum address: {e}"),
     }
 
     // The second way to generate an Ethereum address is to use the Keccak-256 hash function.
